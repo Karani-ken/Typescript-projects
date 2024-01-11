ee5f6bcb9becf722e42a3682c373eb985ee2ec62
karani-ken <academice79@gmail.com>
2024-01-11 16:26:08 +0300

    done with objects

---
+++ 02Basics/myObjects.ts
@@ -0,0 +1,31 @@
+type User={
+    readonly _id:number;
+    name:string;
+    email:string;
+    isActive:boolean
+    creditcard?:number;
+}
+
+/*function createUser(user:User):User{
+    return {name:"",email:"",isActive:true};
+    }
+createUser({name:"",email:"",isActive:true})*/
+
+let myUser : User ={
+    _id: 1234,
+    name:"kenneth",
+    email:"ken@k.com",
+    isActive: false
+}
+
+type cardNumber ={
+    cardnumber: number
+}
+type cardDate = {
+    carddate: string
+}
+
+type cardCvv = {
+    cvv: number
+}
+type cardDetails = cardNumber & cardDate & cardCvv
